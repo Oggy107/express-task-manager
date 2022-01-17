2c4ef3fcf17e617967305e90b3d96c7455fc6205
Oggy107 <urmalsingh10@gmail.com>
2022-01-17 11:19:00 +0530

    Added: validation

--- controller/api-v1/tasks.js
+++ controller/api-v1/tasks.js
@@ -3,37 +3,51 @@ const task = require('../../models/tasks');
 const getTasks = (req, res) => {
     task.find({}).then(tasks => {
         res.json({success: true, data: tasks});
-    }).catch((error) => {
+    }).catch(error => {
         res.status(404).json({success: false, msg: error.message});
     })
 };
 
 const getSingleTask = (req, res) => {
-    res.send("Single task");
+    task.findById(req.params.id).then(task => {
+        res.json({success: true, data: task});
+    }).catch(error => {
+        res.status(404).json({success: false, msg: error.message});
+    })
 };
 
 const createTask = (req, res) => {
-    if (!req.body.name)
-    {
-        res.status(404).json({sucess: false, msg: "missing name value"});
-        return;
-    }
+    // if (!req.body.name)
+    // {
+    //     res.status(404).json({sucess: false, msg: "missing name value"});
+    //     return;
+    // }
 
-    task.create({name: req.body.name, completed: false}).then((task) => {
+    task.create({name: req.body.name}).then(task => {
         res.json({success: true, task});
-    }).catch((error) => {
+    }).catch(error => {
         res.status(404).json({success: false, msg: error.message});
     });
 };
 
 const updateTask = (req, res) => {
-    res.send("Update task");
+    if (!req.body.name || !req.body.completed)
+    {
+        res.status(404).json({sucess: false, msg: "values for name and completed must be supplied"});
+        return;
+    }
+
+    task.updateOne({_id: req.params.id}, {name: req.body.name, completed: req.body.completed}).then(result => {
+        res.json({success: true, result});
+    }).catch(error => {
+        res.status(404).json({success: false, msg: error.message});
+    })
 };
 
 const deleteTask = (req, res) => {
-    task.deleteOne({_id: req.params.id}).then((result) => {
-        res.json({success: true, deletedCount: result.deletedCount});
-    }).catch((error) => {
+    task.deleteOne({_id: req.params.id}).then(result => {
+        res.json({success: true, result});
+    }).catch(error => {
         res.status(404).json({success: false, msg: error.message});
     })
 };
--- models/tasks.js
+++ models/tasks.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: {
+        type: String,
+        required: [true, 'Must be provided'],
+        maxlength: [20, 'Can not be more than 20 characters']
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
 module.exports =  mongoose.model('task', taskSchema);
