358836feedcd2e01e814252e4a7d2f5cde636ded
Oggy107 <urmalsingh10@gmail.com>
2022-01-17 11:26:57 +0530

    Updated: response status codes

--- controller/api-v1/tasks.js
+++ controller/api-v1/tasks.js
@@ -4,7 +4,7 @@ const getTasks = (req, res) => {
     task.find({}).then(tasks => {
         res.json({success: true, data: tasks});
     }).catch(error => {
-        res.status(404).json({success: false, msg: error.message});
+        res.status(500).json({success: false, msg: error.message});
     })
 };
 
@@ -12,7 +12,7 @@ const getSingleTask = (req, res) => {
     task.findById(req.params.id).then(task => {
         res.json({success: true, data: task});
     }).catch(error => {
-        res.status(404).json({success: false, msg: error.message});
+        res.status(400).json({success: false, msg: error.message});
     })
 };
 
@@ -24,23 +24,23 @@ const createTask = (req, res) => {
     // }
 
     task.create({name: req.body.name}).then(task => {
-        res.json({success: true, task});
+        res.status(201).json({success: true, task});
     }).catch(error => {
-        res.status(404).json({success: false, msg: error.message});
+        res.status(400).json({success: false, msg: error.message});
     });
 };
 
 const updateTask = (req, res) => {
     if (!req.body.name || !req.body.completed)
     {
-        res.status(404).json({sucess: false, msg: "values for name and completed must be supplied"});
+        res.status(400).json({sucess: false, msg: "values for name and completed must be supplied"});
         return;
     }
 
     task.updateOne({_id: req.params.id}, {name: req.body.name, completed: req.body.completed}).then(result => {
         res.json({success: true, result});
     }).catch(error => {
-        res.status(404).json({success: false, msg: error.message});
+        res.status(400).json({success: false, msg: error.message});
     })
 };
 
@@ -48,7 +48,7 @@ const deleteTask = (req, res) => {
     task.deleteOne({_id: req.params.id}).then(result => {
         res.json({success: true, result});
     }).catch(error => {
-        res.status(404).json({success: false, msg: error.message});
+        res.status(400).json({success: false, msg: error.message});
     })
 };
 
